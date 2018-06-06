a13790615143f61cb142a2f974599297009fe0a0
Sonia Wadji <soniawadji@gmail.com>
2018-06-06 22:40:18 +0530

    Commit 1.3 Make square width dynamic..Done Need to change based on seekbar

--- app/src/main/java/drawingboard/sonia/com/drawingboard/DrawingView.kt
+++ app/src/main/java/drawingboard/sonia/com/drawingboard/DrawingView.kt
@@ -13,9 +13,16 @@ class DrawingView : View {
 
     private var xDown = 0f
     private var yDown = 0f
-    var width = 200.0f
-    var height = 200.0f
-    var strokeWidth = 10.0f
+    /**
+     * Setting minimum length as 200f
+     * */
+    var squareLength = 200.0f
+        set(value) {
+            if (value >= squareLength) {
+                field = value
+            }
+        }
+    private val strokeWidth = 10.0f
     private var squareList = ArrayList<Square>()
     private lateinit var mPaint: Paint
     private var touched = false
@@ -44,7 +51,6 @@ class DrawingView : View {
         if (touched) {
             for (square in squareList) {
                 canvas.drawRect(square.topLeft, square.topRight, square.bottomLeft, square.bottomRight, mPaint)
-                // canvas.drawRect(xDown, yDown, xDown + width, yDown + height, mPaint)
             }
         }
     }
@@ -55,7 +61,7 @@ class DrawingView : View {
             MotionEvent.ACTION_DOWN -> {
                 xDown = event.x
                 yDown = event.y
-                squareList.add(Square(xDown, yDown, xDown + width, yDown + width))
+                squareList.add(Square(xDown, yDown, xDown + squareLength, yDown + squareLength))
             }
             MotionEvent.ACTION_UP -> {
             }
